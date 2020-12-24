5392abf6807bd83db6ddc899eceefd50b1bde9fd
Stephane Liu <sliu@sjliu.com>
2020-12-23 19:46:28 -1000

    Fix: Rubocop offense

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-workers_count = Integer(ENV.fetch('WEB_CONCURRENCY', 2))
-threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+workers_count = Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 
 workers workers_count
 threads threads_count, threads_count
@@ -9,8 +9,8 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV.fetch('PORT', 3000)
-environment ENV.fetch('RAILS_ENV', 'development')
+port        ENV.fetch("PORT", 3000)
+environment ENV.fetch("RAILS_ENV", "development")
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
